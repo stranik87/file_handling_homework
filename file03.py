383b1db003e7cfd04b5ddd4fc8888d3b3a9ff747
stranik87 <tulkin.boynazarov@gmail.com>
2023-04-02 05:55:54 +0300

    file03

--- file03.py
+++ file03.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    f = open(data).read()
+    print(f)
+    a = []
+    for i in f:
+        if i.isdigit():
+            a.append(int(i))
     
+        
 # Read data from file
+    return a
+print(main("txt_file/data03.txt"))
