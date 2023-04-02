63f07d004776e93300b7235c4b5a02f2556d4bd7
stranik87 <tulkin.boynazarov@gmail.com>
2023-04-02 17:15:50 +0300

    file06

--- file06.py
+++ file06.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    a = []
+    f = open(data).read()
+    for i in f.split():
+        
+        a.append(len(i) )
+    
+    print(a)
     
 # Read data from file
+main("txt_file/data06.txt")
