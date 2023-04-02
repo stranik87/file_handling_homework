6c555957739e88fdfb3914d3adb6433dd00c6f10
stranik87 <tulkin.boynazarov@gmail.com>
2023-04-02 17:38:56 +0300

    file09

--- file09.py
+++ file09.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    a = []
+    f = open(data).read()
+    for i in f:
+        if i.isdigit():
+            a.append(int(i))
+    b = min(a)
+    print(b)
 # Read data from file
+main("txt_file/data09.txt")
