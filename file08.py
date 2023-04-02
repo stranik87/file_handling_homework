04febb19402b868e0cbe33db012a2c1498c8b6d5
stranik87 <tulkin.boynazarov@gmail.com>
2023-04-02 17:32:12 +0300

    file08

--- file08.py
+++ file08.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    a = []
+    f = open(data).read()
+    for i in f:
+        if i.isdigit():
+            a.append(int(i))
+    b = max(a)
+    print(b)
 # Read data from file
+main("txt_file/data08.txt")
