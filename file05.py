0987e82d58a5994439fface38342d8c9654d7495
stranik87 <tulkin.boynazarov@gmail.com>
2023-04-02 06:20:28 +0300

    file05

--- file05.py
+++ file05.py
@@ -6,5 +6,24 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    x = []
+    countNum = 0
+    countStr = 0
+    f = open(data).read()
+    for item in f:
+        if item.isdigit():
+            countNum += 1
+            
+        else:
+            countStr += 1
+    
+
+    x.append(countNum)
+    x.append(countStr)
+    # print(countNum,countStr,x)
+    
+    print(x)
+    return x
     
 # Read data from file
+main("txt_file/data05.txt")
