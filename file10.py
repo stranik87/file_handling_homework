3fc8d51f26b73d512e6b04946c3207cbf719369c
stranik87 <tulkin.boynazarov@gmail.com>
2023-04-03 05:04:21 +0300

    file10

--- file10.py
+++ file10.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    a = []
+    f = open(data).read()
+    for i in f.split("\n"):  
+        a.append(len(i))
+        b = max(a)
 
+    
+    print(b)
 # Read data from file
+
+main("txt_file/data10.txt")
+
